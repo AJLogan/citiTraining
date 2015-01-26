130247ef068af9eecc7d6071c42861221745fb4a
Andrew Logan <andrew@Argon.local>
2015-01-26 10:44:16 +0000

    added accountNumber to bank account
    Signed-off-by: Andrew Logan <andrew@Argon.local>

--- Test/src/basics/BankAccount.java
+++ Test/src/basics/BankAccount.java
@@ -1,6 +1,7 @@
 package basics;
 
 public class BankAccount {
+	public int accountNumber;
 	public double balance;
 
 }
--- Test/src/basics/UseBank.java
+++ Test/src/basics/UseBank.java
@@ -9,13 +9,15 @@ public class UseBank {
 		int n = sc.nextInt();
 		System.out.println("Enter opening Balance: ");
 		BankAccount ba = new BankAccount();
+		ba.accountNumber = n;
 		ba.balance = sc.nextDouble();
+	
 		
 		System.out.println(n + " cubed is " + myCube(n));
 		System.out.println(n + " = " + n); //Primitives passed by value so do not change when used in methods
 		
 		System.out.println("Bank Account balance has changed...");
-		System.out.println("Account Number: " + n);
+		System.out.println("Account Number: " + ba.accountNumber);
 		System.out.println("Old Balance: " + ba.balance + " | New Balance: " + setBalance(ba));
 		
 		System.out.println("ba.balance = " + ba.balance); //reference types are passed by reference so do change when used in methods
