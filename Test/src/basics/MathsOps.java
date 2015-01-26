88da5aaf7e0824f1a8eb662b8782d7896f94f99d
Andrew Logan <andrew@Argon.local>
2015-01-26 10:56:50 +0000

    fixed factorial

--- Test/src/basics/MathsOps.java
+++ Test/src/basics/MathsOps.java
@@ -2,9 +2,10 @@ package basics;
 
 public class MathsOps {
 	public static double factorial(double n) {
-		for (int i = 1; i <= n; i++) {
-			n *= i;
-			}
-		return n;
+		if (n == 1 || n == 0) {
+			return 1;
+			} else {
+				return n * factorial(n - 1);
+				}
 		}
 	}
