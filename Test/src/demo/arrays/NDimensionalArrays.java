dfa0a6f06f8b2d5b3dadb1cc30c06223f08be54b
AJLogan <andrew@ajlogan.net>
2015-01-27 14:52:38 +0000

    n dimensional arrays

---
+++ Test/src/demo/arrays/NDimensionalArrays.java
@@ -0,0 +1,21 @@
+package demo.arrays;
+
+public class NDimensionalArrays {
+
+	public static void main(String[] args) {
+		demo2DArray();
+	}
+
+	private static void demo2DArray() {
+		int[][] tables = { { 1, 2, 3, 4, 5, 6 },
+				{ 2, 4, 6, 8, 10, 12, 14, 16, 18 }, { 3, 6, 9, 12, 15, 18 } };
+		// Using a double for loop, output the array
+		for (int i = 0; i < tables.length; i++) {
+			for (int j = 0; j < tables[i].length; j++) {
+				System.out.print(tables[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
