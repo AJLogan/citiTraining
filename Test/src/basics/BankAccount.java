2f07b4c335188d4dd37b983a2fabe27b587af57d
AJLogan <andrew@ajlogan.net>
2015-01-27 01:04:14 +0000

    no message

--- Test/src/basics/BankAccount.java
+++ Test/src/basics/BankAccount.java
@@ -1,6 +1,31 @@
 package basics;
 
 public class BankAccount {
-	public int accountNumber;
-	public double balance;
+	private int accountNumber;
+	private double balance;
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
+	public double getBalance() {
+		return this.balance;
+	}
+	
+	public void deposit(double amount) {
+		this.balance = balance + amount;
+	}
+	
+//	overloaded deposit method to deposit a float
+	public void deposit(float balance){
+		this.balance = balance + balance;
+	}
+	
+	public void withdraw(double amount) {
+		balance = balance - amount;
+	}
 }
--- Test/src/basics/UseBank.java
+++ Test/src/basics/UseBank.java
@@ -9,20 +9,21 @@ public class UseBank {
 		int n = sc.nextInt();
 		System.out.println("Enter opening Balance: ");
 		BankAccount ba = new BankAccount();
-		ba.accountNumber = n;
-		ba.balance = sc.nextDouble();
-	
+		ba.setAccountNumber(n);
+		ba.deposit(sc.nextDouble());
 		
 		System.out.println(n + " cubed is " + myCube(n));
 		System.out.println(n + " = " + n); //Primitives passed by value so do not change when used in methods
 		
 		System.out.println("Bank Account balance has changed...");
-		System.out.println("Account Number: " + ba.accountNumber);
-		System.out.println("Old Balance: " + ba.balance + " | New Balance: " + setBalance(ba));
+		System.out.println("Account Number: " + ba.getAccountNumber());
+		System.out.println("Old Balance: " + ba.getBalance() + " | New Balance: " + setBalance(ba));
 		
-		System.out.println("ba.balance = " + ba.balance); //reference types are passed by reference so do change when used in methods
+		System.out.println("ba.balance = " + ba.getBalance()); //reference types are passed by reference so do change when used in methods
 		
 		sc.close();
+		
+		System.out.println(ba.toString());
 	}
 	
 	private static int myCube(int n){
@@ -31,7 +32,7 @@ public class UseBank {
 	}
 	
 	private static double setBalance(BankAccount ba){
-		ba.balance += 100;
-		return ba.balance;
+		ba.deposit(100);
+		return ba.getBalance();
 	}
 }
