6bcf51b7235d2d50589a1a4c5d11e65e0c5ee92a
AJLogan <andrew@ajlogan.net>
2015-01-27 09:55:47 +0000

    added constructors and TestBank to create an account to set up from scratch

--- Test/src/basics/BankAccount.java
+++ Test/src/basics/BankAccount.java
@@ -1,31 +1,59 @@
 package basics;
 
 public class BankAccount {
+	private String accountHolder;
 	private int accountNumber;
 	private double balance;
+
+	
+	public BankAccount() {
+		this.accountHolder = "";
+		this.accountNumber = 0;
+		this.balance = 0;
+	}
+	
+	public BankAccount(String accountHolder, int accountNumber, double balance) {
+		this.accountHolder = accountHolder;
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+
+	public String getAccountHolder(){
+		return accountHolder;
+	}
+	
+	public void setAccountHolder(String accountHolder){
+		this.accountHolder = accountHolder;
+	}
 	
 	public int getAccountNumber() {
 		return accountNumber;
 	}
-	
+
 	public void setAccountNumber(int accountNumber) {
 		this.accountNumber = accountNumber;
 	}
-	
+
 	public double getBalance() {
 		return this.balance;
 	}
-	
+
 	public void deposit(double amount) {
 		this.balance = balance + amount;
 	}
-	
-//	overloaded deposit method to deposit a float
-	public void deposit(float balance){
-		this.balance = balance + balance;
+
+	// overloaded deposit method to deposit a float
+	public void deposit(float amount) {
+		this.balance = balance + amount;
 	}
-	
+
 	public void withdraw(double amount) {
 		balance = balance - amount;
+	}
+
+	@Override
+	public String toString() {
+		String str = String.format("Account Holder (%s) has an Account %s with a balance %.2f", accountHolder, accountNumber, balance);
+		return str;
 	}
 }
--- Test/src/basics/UseBank.java
+++ Test/src/basics/UseBank.java
@@ -8,28 +8,35 @@ public class UseBank {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		System.out.println("Enter opening Balance: ");
-		BankAccount ba = new BankAccount();
-		ba.setAccountNumber(n);
-		ba.deposit(sc.nextDouble());
+//		ba.setAccountNumber(n);
+		double balance = sc.nextDouble();
+		BankAccount ba = new BankAccount("Test", n, balance);
+//		BankAccount ba = new BankAccount(); << Default Constructor Usage >>
 		
-		System.out.println(n + " cubed is " + myCube(n));
-		System.out.println(n + " = " + n); //Primitives passed by value so do not change when used in methods
+//		System.out.println(n + " cubed is " + myCube(n));
+//		System.out.println(n + " = " + n); //Primitives passed by value so do not change when used in methods
 		
 		System.out.println("Bank Account balance has changed...");
+		
 		System.out.println("Account Number: " + ba.getAccountNumber());
-		System.out.println("Old Balance: " + ba.getBalance() + " | New Balance: " + setBalance(ba));
+		System.out.println("Old Balance: " + ba.getBalance());
+		setBalance(ba);
+		System.out.println("New Balance: " + ba.getBalance());
 		
 		System.out.println("ba.balance = " + ba.getBalance()); //reference types are passed by reference so do change when used in methods
 		
 		sc.close();
 		
 		System.out.println(ba.toString());
+		
+		ba.withdraw(100);
+		System.out.println("ba.balance = " + ba.getBalance()); //reference types are passed by reference so do change when used in methods
 	}
 	
-	private static int myCube(int n){
-		n = n*n*n;
-		return n;
-	}
+//	private static int myCube(int n){
+//		n = n*n*n;
+//		return n;
+//	}
 	
 	private static double setBalance(BankAccount ba){
 		ba.deposit(100);
